6247e55e7a353ffb5858359dc2a4771d5aa2fbeb
yossefmagdy <y.f.magdy@gmail.com>
2023-02-11 23:21:09 +0200

    Edit in Validation

--- js/Home.js
+++ js/Home.js
@@ -1,7 +1,7 @@
 var Admin=JSON.parse(localStorage.getItem("Username"));
 var Logout=document.getElementById("logout");
 
-document.getElementById("welcome").innerHTML='<h2>Welcome '+Admin+'</h2><button onclick="gotophoto()" class="btn btn-primary mt-4">Photo Library</button><button onclick="gotoBookmark()" class="btn btn-primary mt-4 ms-md-2">Book Marker</button>';
+document.getElementById("welcome").innerHTML='<h2>Welcome '+Admin+'</h2><button onclick="gotophoto()" class="btn btn-primary mt-4">Photos</button><button onclick="gotoBookmark()" class="btn btn-primary mt-4 ms-md-2">Book Marker</button>';
 
 Logout.addEventListener("click",goback);
 
